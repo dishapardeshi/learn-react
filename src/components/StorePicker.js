8634d1acb94c1290a75068de2fd6caef473ada9f
Disha Pardeshi <disha.pardeshi@gmail.com>
2017-07-02 14:14:53 -0400

    refactored main component away from index.js

---
+++ src/components/StorePicker.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+class StorePicker extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    );
+  }
+
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
-
-import './css/style.css';
-
 import registerServiceWorker from './registerServiceWorker';
 
-class StorePicker extends React.Component {
-
-  render(){
-    return (
-      <div>
-        <p>Hello</p>
-      </div>
-    );
-  }
-
-}
+import './css/style.css';
+import StorePicker from './components/StorePicker';
 
 render(<StorePicker />, document.getElementById('root'));
 
