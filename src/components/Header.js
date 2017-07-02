f746fd2c8719bcf1f85f36f5dd911e165813de2b
Disha Pardeshi <disha.pardeshi@gmail.com>
2017-07-02 17:22:48 -0400

    added header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,16 @@ export class Header extends Component {
   render() {
     return (
       <div>
-        <p>Header</p>
+      <header className="top">
+      <h1>Catch
+        <span className="ofThe">
+          <span className="of">of</span>
+          <span className="the">the</span>
+        </span>
+        Day
+      </h1>
+      <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
       </div>
     );
   }
