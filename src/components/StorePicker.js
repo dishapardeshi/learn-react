cbb8ec2204752d8b7b3da5e8e5fff3e13ed4efbd
Disha Pardeshi <disha.pardeshi@gmail.com>
2017-07-02 19:27:00 -0400

    routing using history push from store selection to main app

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,17 +1,39 @@
 import React from 'react';
 
+import { getFunName } from '../helpers';
+import { PropTypes } from 'prop-types';
+
 class StorePicker extends React.Component {
 
-  render(){
+  constructor(){
+    super();
+
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  goToStore(event) {
+    event.preventDefault();
+    console.log(`URL is changed`);
+    let storeId = this.storeInput.value;
+    this.context.router.history.push(`/store/${storeId}`);
+  }
+
+  render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={(input) => {this.storeInput = input}}/>
         <button type="submit">Visit Store</button>
       </form>
     );
   }
 
+}
+
+StorePicker.contextTypes = {
+  router: PropTypes.object
 }
 
 export default StorePicker;
