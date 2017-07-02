14b99366c00b6a2870488df2189beddd9004a207
Disha Pardeshi <disha.pardeshi@gmail.com>
2017-07-02 18:05:21 -0400

    added basic routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import './css/style.css';
-//import StorePicker from './components/StorePicker';
+import StorePicker from './components/StorePicker';
 import App from './components/App';
+import {NotFound}  from './components/NotFound';
 
-render(<App />, document.getElementById('main'));
+const Root = () => {
+  return(
+    <Router>
+      <Switch>
+        <Route exact path="/" component={StorePicker}></Route>
+        <Route path="/store/:storeId" component={App}></Route>
+        <Route component={NotFound}></Route>
+      </Switch>
+    </Router>
+  );
+};
+
+render( <Root/> , document.getElementById('main'));
 
 registerServiceWorker();
