6fb4be58d1d3dd2adc6a71f42354ae0622effb46
Disha Pardeshi <disha.pardeshi@gmail.com>
2017-07-02 17:29:32 -0400

    refactor header as component function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 
-export class Header extends Component {
-  render() {
+export const Header = (props) => {
     return (
       <div>
       <header className="top">
-      <h1>Catch
-        <span className="ofThe">
-          <span className="of">of</span>
-          <span className="the">the</span>
-        </span>
-        Day
-      </h1>
-      <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+        <h1>Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
+        <h3 className="tagline"><span>{props.tagline}</span></h3>
       </header>
       </div>
     );
-  }
-}
+};
