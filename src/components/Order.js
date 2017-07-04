0ec9878d01da77a1a60296c53f13e5dfaeb4fee9
Disha Pardeshi <disha.pardeshi@gmail.com>
2017-07-04 08:08:12 -0400

    display order details

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 
+import {formatPrice} from '../helpers';
+
 export class Order extends Component {
+
+  constructor(){
+    super()
+    this.renderOrder = this.renderOrder.bind(this);
+  }
+
+  renderOrder(key){
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+
+    if( !fish || fish.status === 'unavailable'){
+      return (<li key={key}>Sorry, {fish ? fish.name : 'Fish'} is no longer available! </li>)
+    }
+    return (
+      <li key={key}>
+        <span>{count} lbs {fish.name}</span>
+        <span className="price">{formatPrice(fish.price * count)}</span>
+      </li>
+    );
+  }
+
   render() {
     const orderIds = Object.keys(this.props.order);
     const total = orderIds
@@ -17,8 +40,13 @@ export class Order extends Component {
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
-        <p>{orderIds}</p>
-        <p>{total}</p>
+        <ul className="order">
+          { orderIds.map(this.renderOrder) }
+          <li className="total">
+          <strong>Total:</strong>
+          {formatPrice(total)}
+          </li>
+        </ul>
       </div>
     );
   }
