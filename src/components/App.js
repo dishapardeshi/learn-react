3641653f2798735a511acca3ceaa7f6632d2577a
Disha Pardeshi <disha.pardeshi@gmail.com>
2017-07-04 07:48:44 -0400

    order details to order comp

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
   }
 
   addFish(fish){
@@ -37,6 +38,13 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   }
 
+  addToOrder(key) {
+    console.log('add to Order');
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({order: order});
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -46,11 +54,11 @@ class App extends React.Component {
             {
               Object
               .keys(this.state.fishes)
-              .map( key => <Fish key={key} details={this.state.fishes[key]}/>)
+              .map( key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
             }
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     );
--- src/components/Order.js
+++ src/components/Order.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 
 export class Order extends Component {
   render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds
+      .reduce((prevTotal, key) => {
+        const fish = this.props.fishes[key];
+        const count = this.props.order[key];
+        const isAvailable = fish.status === 'available';
+        if(isAvailable){
+          return prevTotal + (count * fish.price || 0);
+        }
+        return prevTotal;
+    },0);
+
     return (
-      <div>
-        <p>Order</p>
+      <div className="order-wrap">
+        <h2>Your Order</h2>
+        <p>{orderIds}</p>
+        <p>{total}</p>
       </div>
     );
   }
